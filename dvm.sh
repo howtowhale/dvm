4bee10ae431b42bc748666a6e6dc58adb8e85224
Kyle Kelley <rgbkrk@gmail.com>
2015-09-17 17:27:59 -0500

    Stub out dvm.sh

---
+++ dvm.sh
@@ -0,0 +1,15 @@
+# Docker Version Manager
+# Implemented as a POSIX-compliant function
+# Should work on sh, dash, bash, ksh, zsh
+# To use, source this file from your bash profile
+#
+# Implemented by Kyle Kelley <rgbkrk@gmail.com>
+# Influenced by nvm, via Tim Caswell <tim@creationix.com>
+
+{ # This ensures the entire script is downloaded
+
+DVM_SCRIPT_SOURCE="$_"
+
+
+
+}
