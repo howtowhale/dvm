36f893a9570170828238ae218786a624036ec92a
Kyle Kelley <rgbkrk@gmail.com>
2015-09-18 10:18:43 -0500

    List the help for dvm

--- dvm.sh
+++ dvm.sh
@@ -59,4 +59,61 @@ if [ -z "$DVM_GET_DOCKER_MIRROR" ]; then
   export DVM_GET_DOCKER_MIRROR="https://get.docker.com/builds/"
 fi
 
+## Skippy skip
+
+
+dvm() {
+    if [ $# -lt 1 ]; then
+      dvm help
+      return
+    fi
+
+    local GREP_OPTIONS
+    GREP_OPTIONS=''
+
+    local VERSION
+    local ADDITIONAL_PARAMETERS
+    local ALIAS
+
+    # TODO: help mentions .dvmrc, doesn't exist yet
+
+    case $1 in
+      "help" )
+        echo
+        echo "Docker Version Manager"
+        echo
+        echo "Usage:"
+        echo "  dvm help                              Show this message"
+        echo "  dvm --version                         Print out the latest released version of dvm"
+        echo "  dvm install [-s] <version>            Download and install a <version>, [-s] from source. Uses .dvmrc if available"
+        echo "  dvm uninstall <version>               Uninstall a version"
+        echo "  dvm use <version>                     Modify PATH to use <version>. Uses .dvmrc if available"
+        echo "  dvm run <version> [<args>]            Run <version> with <args> as arguments. Uses .dvmrc if available for <version>"
+        echo "  dvm current                           Display currently activated version"
+        echo "  dvm ls                                List installed versions"
+        echo "  dvm ls <version>                      List versions matching a given description"
+        echo "  dvm ls-remote                         List remote versions available for install"
+        echo "  dvm deactivate                        Undo effects of \`dvm\` on current shell"
+        echo "  dvm alias [<pattern>]                 Show all aliases beginning with <pattern>"
+        echo "  dvm alias <name> <version>            Set an alias named <name> pointing to <version>"
+        echo "  dvm unalias <name>                    Deletes the alias named <name>"
+        echo "  dvm unload                            Unload \`dvm\` from shell"
+        echo "  dvm which [<version>]                 Display path to installed docker version. Uses .dvmrc if available"
+        echo
+        echo "Example:"
+        echo "  dvm install 1.8.1                     Install a specific version number"
+        echo "  dvm use 1.6                           Use the latest available 1.6.x release"
+        echo "  dvm run 1.6.1 -it ubuntu /bin/bash    Run an ubuntu container using Docker 1.6.1"
+        echo "  dvm exec 1.6.1 nginx                  Run the nginx image the PATH pointing to Docker 1.6.1"
+        echo "  dvm alias default 1.8.1               Set default Docker version on a shell"
+        echo
+        echo "Note:"
+        echo "  to remove, delete, or uninstall dvm - remove ~/.dvm"
+        echo
+      ;;
+
+
+}
+
+
 }
