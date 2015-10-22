7e407d95336b7b6900326baf387cc03e4d04e603
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-10-22 13:51:34 -0500

    Add CMD wrapper script

---
+++ dvm.cmd
@@ -0,0 +1,26 @@
+:: Docker Version Manager CMD Wrapper
+:: Implemented as a POSIX-compliant function
+:: To use, add this script's parent directory to your path
+
+@ECHO OFF
+SETLOCAL
+
+SET DVM_DIR=%~dp0
+
+IF NOT EXIST dvm-helper.exe (
+  :: TODO: Download the binary instead of grabbing it from a local build
+  COPY %DVM_DIR%\bin\windows\amd64\dvm-helper.exe %DVM_DIR% > nul
+)
+
+SET DVM_OUTPUT=%DVM_DIR%\.tmp\dvm-output.cmd
+
+IF EXIST %DVM_OUTPUT% (
+  DEL %DVM_OUTPUT%
+)
+
+%DVM_DIR%\dvm-helper.exe --shell cmd %*
+
+IF EXIST %DVM_OUTPUT% (
+  ENDLOCAL
+  CALL %DVM_OUTPUT%
+)
