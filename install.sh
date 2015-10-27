732847583a8fe18d84f4e2e16be3a5330fb07b54
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-10-27 14:28:11 -0500

    Install dvm on Mac/Linux

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+{ # this ensures the entire script is downloaded #
+
+set -e
+
+if [ -z "$DVM_DIR" ]; then
+  DVM_DIR="$HOME/.dvm"
+fi
+
+if [ ! -d "$DVM_DIR" ]; then
+  mkdir $DVM_DIR
+fi
+
+echo "Downloading dvm.sh..."
+curl -s -o $DVM_DIR/dvm.sh https://cdn.rawgit.com/getcarina/dvm/master/dvm.sh
+
+echo "Docker Version Manager (dvm) has been installed to ${DVM_DIR}"
+echo "Add the following command to your bash profile (e.g. ~/.bashrc or ~/.bash_profile) complete the installation:"
+echo ""
+echo "\tsource ${DVM_DIR}/dvm.sh"
+
+}
