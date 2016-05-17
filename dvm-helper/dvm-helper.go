624556b7534b86a002d2b03710be361301a1b0dd
Andre de Oliveira Fernandes <andre.oliveira@bcb.gov.br>
2016-05-17 19:53:33 -0300

    mirror

--- dvm-helper/dvm-helper.go
+++ dvm-helper/dvm-helper.go
@@ -294,10 +294,12 @@ func install(version dockerversion.Version) {
 }
 
 func buildDownloadURL(version dockerversion.Version) string {
-  mirrorURL := os.Getenv("MIRROR_URL")
-  if mirrorURL == "" {
-	  mirrorURL := "https://get.docker.com/builds"
-  }
+	tmpURL := os.Getenv("MIRROR_URL")
+	if tmpURL == "" {
+		mirrorURL := "https://get.docker.com/builds"
+	} else {
+		mirrorURL := tmpURL
+	}
 	dockerVersion := version.SemVer.String()
 	if version.IsExperimental() {
 		writeDebug("Downloading from experimental builds mirror")
