4992d3eb21937265a53cbf730119f973beca52c2
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-06-30 10:36:36 -0500

    saving rando changes

--- install.ps1
+++ install.ps1
@@ -1,48 +1,4 @@
-$ErrorActionPreference = "Stop"
-
-function downloadDvm([string] $dvmDir) {
-  $webClient = New-Object net.webclient
-
-  echo "Downloading dvm.ps1..."
-  $webClient.DownloadFile("https://download.getcarina.com/dvm/latest/dvm.ps1", "$dvmDir\dvm.ps1")
-
-  echo "Downloading dvm.cmd..."
-  $webClient.DownloadFile("https://download.getcarina.com/dvm/latest/dvm.cmd", "$dvmDir\dvm.cmd")
-
-  echo "Downloading dvm-helper.exe..."
-  $tmpDir = Join-Path $dvmDir .tmp
-  if( !(Test-Path $tmpDir) ) {
-    mkdir $tmpDir > $null
-  }
-
-  # Ensure dvm-helper directory exists
-  $dvmHelperDir = Join-Path $dvmDir dvm-helper
-  if( !(Test-Path $dvmHelperDir ) ) {
-    mkdir $dvmHelperDir > $null
-  }
-
-
-  # Detect x86 vs. x64
-  if( [System.Environment]::Is64BitOperatingSystem ) { $arch = "x86_64" } else { $arch = "i386"}
-
-  # Download latest release
-  $webClient.DownloadFile("https://download.getcarina.com/dvm/latest/Windows/$arch/dvm-helper.exe", "$tmpDir\dvm-helper.exe")
-  $webClient.DownloadFile("https://download.getcarina.com/dvm/latest/Windows/$arch/dvm-helper.exe.sha256", "$tmpDir\dvm-helper.exe.256")
-
-  # Verify the binary was downloaded successfully
-  $checksum = (cat $tmpDir\dvm-helper.exe.256).Split(' ')[0]
-  $hash = (Get-FileHash $tmpDir\dvm-helper.exe).Hash
-  if([string]::Compare($checksum, $hash, $true) -ne 0) {
-    $host.ui.WriteErrorLine("DANGER! The downloaded dvm-helper binary, $tmpDir\dvm-helper.exe, does not match its checksum!")
-    $host.ui.WriteErrorLine("CHECKSUM: $checksum")
-    $host.ui.WriteErrorLine("HASH: $hash")
-    return 1
-  }
-
-  mv -force "$tmpDir\dvm-helper.exe" "$dvmDir\dvm-helper\dvm-helper.exe"
-}
-
-function installDvm()
+function installMagic()
 {
   $dvmDir = $env:DVM_DIR
   if( $dvmDir -eq $null ) {
@@ -67,8 +23,8 @@ function installDvm()
 }
 
 if($PSVersionTable -eq $null -or $PSVersionTable.PSVersion.Major -lt 4){
-  Write-Output "dvm requires PowerShell version 4 or higher."
+  Write-Output "magic requires PowerShell version 4 or higher."
   Exit 1
 }
 
-installDvm
+iwr -Uri https:// -OutFile $output
