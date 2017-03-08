1d85f5eb0ea4ce5b1417458597aca68a5bd89604
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-03-08 12:46:11 -0600

    Fix travis deploy to gh-pages

--- script/deploy-gh-pages.sh
+++ script/deploy-gh-pages.sh
@@ -2,10 +2,14 @@
 set -euo pipefail
 
 rm -fr _deploy/ &> /dev/null
-git clone --branch gh-pages --depth 1 https://github.com/howtowhale/dvm.git _deploy
-cp -R bin/dvm/$(VERSION) _deploy/downloads/
-cp -R bin/dvm/$(PERMALINK) _deploy/downloads/
+git clone --branch gh-pages --depth 1 git@github.com:howtowhale/dvm.git _deploy
+
+cp -R bin/dvm/$VERSION _deploy/downloads/
+cp -R bin/dvm/$PERMALINK _deploy/downloads/
 cd _deploy/
 git add downloads/
-git commit --author "Travis CI <travis@travis-ci.org>" -m "Publish $VERSION from Travis Build #$TRAVIS_BUILD_NUMBER"
-#PKEY=.travis.deploy.pem git push
+
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+git commit -m "Publish $VERSION from Travis Build #${TRAVIS_BUILD_NUMBER:-1}"
+git push
